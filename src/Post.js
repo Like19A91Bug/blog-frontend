5d4b66e0da4b8012196a4dcb4f30c4d93bd2f29a
Like19A91Bug <bugnificent.tk@gmail.com>
2023-09-05 19:39:35 -0400

    start

---
+++ src/Post.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+const Post = ({ data }) => {
+    return (
+        <div className="col-4">
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{data.title}</h5>
+                    <div className="card-text">
+                        <div className="author">{data.tagline}</div>
+                        <div className="timestamp">
+                            {new Date(data.updatesAt).toLocaleTimeString()}
+                        </div>
+                        <p>
+                            {data.content.slice(0, 100)}
+                            {data.content.length > 100 ? "..." : ""}
+                        </p>
+                    </div>
+                    <Link to={`read/${data.id}`} className="btn btn-primary">
+                        Read more
+                    </Link>
+                </div>
+            </div>
+            {/* <!-- end of my card--> */}
+        </div>
+    );
+};
+
+export default Post;
